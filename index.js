fc8739a038dbe52e78378d433b6c4588ab7a8a98
rubelahme <you@example.com>
2022-04-09 00:40:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const port = 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://rubel:5QsNjZfNrr4vuNtQ@cluster0.vb3lh.mongodb.net/ErosUpdate?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {
+  const collection = client.db("ErosUpdate").collection("Update");
+  const collections = client.db("ErosUpdate").collection("image");
+
+  app.post("/email", (req, res) => {
+    const user = req.body;
+    collection.insertOne(user).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.post("/images", (req, res) => {
+    const user = req.body;
+    collections.insertOne(user).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/emails", (req, res) => {
+    collection.find().toArray((err, result) => {
+      res.send(result);
+    });
+  });
+
+  app.get("/image", (req, res) => {
+    collections.find().toArray((err, result) => {
+      res.send(result);
+    });
+  });
+
+  app.delete("/delete/:id", (req, res) => {
+    collection.deleteOne({ _id: ObjectId(req.params.id) }).then((result) => {
+      res.send(result.deletedCount > 0);
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
